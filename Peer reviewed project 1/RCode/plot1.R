8fba063a6bdd6092d91d45cd60e10c6d5269980f
edep16 <edep16@gmail.com>
2017-09-09 18:29:35 -0500

    corrected mistakes
    corrected some mistakes in codes for plot3 and plot4

--- Peer reviewed project 1/RCode/plot1.R
+++
@@ -1,7 +0,0 @@
-#Read relevant data 
-power_data <- read.table("./exdata_data_household_power_consumption/household_power_consumption.txt", na.strings = "?", nrows = 2880, col.names = c('Date','Time','Global_active_power','Global_reactive_power','Voltage','Globale_intensity','Sub_metering_1','Sub_metering_2','Sub_metering_3'), sep = ';', skip = 66637)
-
-#Generate the plot1
-par(mfrow = c(1,1))
-
-with(power_data, hist(Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)"))
--- plot3.R
+++ plot3.R
@@ -10,5 +10,5 @@ plot(dates_of_measurement, power_data$Sub_metering_1, xlab = '', ylab = "Engergy
 lines(dates_of_measurement, power_data$Sub_metering_1)
 lines(dates_of_measurement, power_data$Sub_metering_2, col = "red")
 lines(dates_of_measurement, power_data$Sub_metering_3, col = "blue")
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), pch = c('_', '_', '_'), col = c("black", "red", "blue"))
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = c(1, 1, 1), col = c("black", "red", "blue"))
 
